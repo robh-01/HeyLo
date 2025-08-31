9b8a77ec2499a369b09da69ea0bd96b1c6f1c1a2
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-08-31 10:06:37 +0545

    Make users able to communicate through global msgs

--- server/src/socket_server/socket.ts
+++ server/src/socket_server/socket.ts
@@ -1,17 +1,33 @@
 import { Server } from "socket.io";
 import http from "http";
+import jwt from "jsonwebtoken";
+
+import { getUserById } from "../db/userQueries.js";
 
 export function setupSocket(httpServer: http.Server) {
   const io = new Server(httpServer, {
     serveClient: false,
   });
 
-
-  io.on("connection", (socket) => {
-    console.log("a user connected");
+  io.on("connection", async (socket) => {
     if (socket.handshake.auth.token) {
       //todo: verify token
-      console.log("token:", socket.handshake.auth.token);
+      const token = socket.handshake.auth.token;
+      try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+
+        // token is valid
+        const user = await getUserById(decoded.sub as string)
+        if (user) {
+          console.log(`${user.username} is online.`);
+        }
+
+      } catch (err) {
+        // invalid token
+        console.log("invalid token:", err);
+        socket.disconnect();
+        return;
+      }
     } else {
       console.log("no token provided");
       socket.disconnect();
@@ -20,6 +36,10 @@ export function setupSocket(httpServer: http.Server) {
 
     socket.on("disconnect", () => {
       console.log("user disconnected");
+    });
+
+    socket.on("global message", (msg) => {
+      socket.broadcast.emit("global message", msg);
     });
   });
 }
