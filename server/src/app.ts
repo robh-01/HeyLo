abb1d2f51709ffdd76d4f263b40988321d8a56b4
Roshan Bhusal <128559779+robh-01@users.noreply.github.com>
2025-09-05 12:21:32 +0545

    feat: add endpoint to validate token + cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,17 @@ import { configPassport } from "./configs/passport.config.js";
 
 import express from "express";
 
+import cors from "cors";
+
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
